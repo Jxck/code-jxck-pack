4dea5f6a0d8dc3bb463cda58616f730eb4d1f5e0
Jxck <jxck@jxck.io>
2024-05-14 02:50:52 +0900

    [ext] add Cloude to api.mjs

--- api.mjs
+++ api.mjs
@@ -1,13 +1,12 @@
 const apiKey = ""
 
-async function proof({ input }) {
+async function ChatGTP({ instruction, input }) {
   const apiUrl = "https://api.openai.com/v1/chat/completions"
   const headers = {
     "Content-Type": "application/json",
     Authorization: `Bearer ${apiKey}`
   }
   const model = "gpt-4-turbo"
-  const instruction = "文章の誤字、脱字、スペルミスを修正してください。"
 
   const res = await fetch(apiUrl, {
     method: "POST",
@@ -21,13 +20,44 @@ async function proof({ input }) {
       temperature: 0
     })
   })
-  return res.json()
+  const json = res.json()
+  return json.choices.at(0).message.content
 }
+
+async function Claude({ instruction, input }) {
+  const url = "https://api.anthropic.com/v1/messages"
+  const headers = {
+    "x-api-key": apiKey,
+    "anthropic-version": "2023-06-01",
+    "content-type": "application/json"
+  }
+  const body = JSON.stringify({
+    model: "claude-3-opus-20240229",
+    max_tokens: 1024,
+    messages: [
+      {
+        role: "user",
+        content: `${instruction}\n${input}`
+      }
+    ]
+  })
+  const res = await fetch(url, {
+    method: "post",
+    headers,
+    body
+  })
+  const json = await res.json()
+  console.log(json)
+  return json.content.at(0).text
+}
+
+const instruction = "文章の誤字、脱字、スペルミスを修正してください。"
 
 const input = `
 この文章は、ところどろ間違っており、できれば習性して欲しいです。特に ChatGTP についての記述は注意したです。
 `.trim()
 
-const result = await proof({ input })
+// const result = await ChatGTP({ instruction, input })
+const result = await Claude({ instruction, input })
 console.log(input)
-console.log(result.choices.at(0).message.content)
+console.log(result)
